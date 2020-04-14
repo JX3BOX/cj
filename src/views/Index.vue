7a83aff56364996540c1b2f195ce5856472f4d73
iRuxu <rx6@qq.com>
2020-04-15 00:05:14 +0800

    i don't know

---
+++ src/views/Index.vue
@@ -0,0 +1,25 @@
+<template>
+    <main class="c-main m-cj-index">
+        222
+        <Info />
+    </main>
+</template>
+
+<script>
+import Info from '@/components/Info.vue';
+    export default {
+        name : 'Index',
+        props:[],
+        data : function(){
+            return {
+                
+            }
+        },
+        computed:{},
+        methods:{},
+        mounted:function(){},
+        components:{
+            Info
+        }
+    }
+</script>
