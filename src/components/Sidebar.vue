1de2d079ef6aa8b316d291062d3bc8cc3b22a490
iRuxu <rx6@qq.com>
2020-07-11 16:53:56 +0800

    边栏效果

--- src/components/Sidebar.vue
+++ src/components/Sidebar.vue
@@ -38,7 +38,7 @@
 
 <script>
 const { JX3BOX } = require("@jx3box/jx3box-common");
-import Bus from '@jx3box/jx3box-common-ui/service/bus'
+import Bus from "@jx3box/jx3box-common-ui/service/bus";
 
 export default {
     name: "Sidebar",
@@ -110,10 +110,11 @@ export default {
             this.old_node = _node;
 
             // 移动端收起边栏
-            if(node.isLeaf){
-                Bus.$emit('toggleLeftSide',false);
+            if (window.innerWidth < 1024) {
+                if (node.isLeaf) {
+                    Bus.$emit("toggleLeftSide", false);
+                }
             }
-
         },
         get_menus(general) {
             let that = this;
