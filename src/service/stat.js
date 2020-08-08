76539f254d4cbf43c4b82c4a72d9834a0639b9f1
iRuxu <rx6@qq.com>
2020-08-08 23:54:44 +0800

    update

--- src/service/stat.js
+++ src/service/stat.js
@@ -1,12 +1,11 @@
 import axios from "axios";
 import { __next } from "@jx3box/jx3box-common/js/jx3box.json";
 
-const stat = __next + 'api/summary-any/'    //TODO:
+const stat = __next + "api/summary-any/";
 // const stat = "/api/summary-any/";
-
 function getStat(id) {
     return axios
-        .get(stat + id + "/stat")
+        .get(stat + 'cj-' + id + "/stat")
         .then((res) => {
             return res.data;
         })
@@ -14,13 +13,28 @@ function getStat(id) {
             console.log(err);
         });
 }
+
+const actions = __next + "api/summary-any/";
+// const actions = "/api/summary-any/";
 function postStat(id) {
-    return axios.get(stat + 'cj-' + id, {
+    return axios.get(actions + 'cj-' + id, {
         params: {
-            type: 'cj',
+            type: "cj",
             actions: "views",
         },
     });
 }
+const rank = __next + 'api/summary/visit/rank'
+// const rank = "/api/summary/visit/rank";
+function getRank() {
+    return axios.get(rank,{
+        params : {
+            postType : 'cj',
+            postAction : 'views',
+            pageSize : 10
+        }
+    });
+}
 
-export { getStat, postStat };
+export { getStat, postStat, getRank };
+
