2dae7596adddca212c8f75d22985b8ea4c9a6ef5
kuguats <445786355@qq.com>
2021-03-30 01:14:05 +0800

    fix

--- src/views/Detail.vue
+++ src/views/Detail.vue
@@ -93,7 +93,7 @@ export default {
                 if (res.code === 200) this.wiki_post = res.data;
                 if (this.wiki_post && this.wiki_post.source) {
                   let pet = this.wiki_post.source.pet;
-                  if (pet.id) postStat('pet', pet.id);
+                  if (pet && pet.id) postStat('pet', pet.id);
                 }
               },
               () => {
