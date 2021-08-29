21d13d2d4d2ddc036b188ac7f70274a127ba1d5d
Kuguats <445786355@qq.com>
2021-08-29 19:23:48 +0800

    fix

--- src/service/axios.js
+++ src/service/axios.js
@@ -1,9 +1,10 @@
 import axios from "axios";
 import { __next } from "@jx3box/jx3box-common/data/jx3box.json";
+import {jx3ClientType} from "@jx3box/jx3box-common/js/utils";
 
 const $http = axios.create({
     withCredentials: true,
-    headers: {'JX3-Client-Type': 2}
+    headers: {'JX3-Client-Type': jx3ClientType()}
 });
 
 const $next = axios.create({
