d72223e3f12e5448c0e9bc98f7f590d29e9f8c61
Kaviilee <jiaweilee95@126.com>
2022-02-10 18:05:51 +0800

    update

--- src/components/AchievementCount.vue
+++ src/components/AchievementCount.vue
@@ -1,36 +1,40 @@
 <template>
-  <div class="m-achievement-count">
-    <div class="u-stat">
-      <span><em>常规成就数</em><b v-text="count.general"></b></span>
-      <span><em>常规资历数</em><b v-text="count.general_point"></b></span>
-      <span><em>五甲成就数</em><b v-text="count.armor"></b></span>
-      <span><em>五甲资历数</em><b v-text="count.armor_point"></b></span>
-      <span><em>收录攻略数</em><b v-text="count.post_count"></b></span>
+    <div class="m-achievement-count">
+        <div class="u-stat">
+            <span v-for="(text, key) in countDict" :key="key">
+                <em>{{ text }}</em>
+                <b>{{ count[key] }}</b>
+            </span>
+        </div>
     </div>
-  </div>
 </template>
 <script>
-  import {getAchievementsTotal} from '../service/achievement';
+import { getAchievementsTotal } from "../service/achievement";
 
-  export default {
+export default {
     name: "AchievementCount",
     data() {
-      return {
-        count: {},
-      };
+        return {
+            count: {},
+            countDict: {
+                general: "常规成就数",
+                general_point: "常规资历数",
+                armor: "五甲成就数",
+                armor_point: "五甲资历数",
+                post_count: "收录攻略数",
+            },
+        };
     },
     created() {
-      // 获取成就统计信息
-      getAchievementsTotal().then(
-          (data) => {
+        // 获取成就统计信息
+        getAchievementsTotal().then((data) => {
             data = data.data;
-            if (data.code === 200) this.count = data.data.count;
-          }
-      );
+            this.count = data.data.count;
+        });
     },
-  };
+};
 </script>
 
 <style lang="less">
-  @import '../assets/css/components/achievement-count.less';
+@import "../assets/css/components/achievement-count.less";
 </style>
