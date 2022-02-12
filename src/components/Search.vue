f471d0d77d577805af0db4a8996c3fded97afbbe
iRuxu <rx6@qq.com>
2022-02-12 22:14:53 +0800

    chore:opt search style

--- src/components/Search.vue
+++ src/components/Search.vue
@@ -1,29 +1,13 @@
 <template>
-    <div
-        class="c-search-bar"
-        :class="$route.name == 'view' ? 'can-return' : ''"
-    >
+    <div class="m-search-bar" :class="$route.name == 'view' ? 'can-return' : ''">
         <div class="m-return">
-            <el-button class="u-return-btn" @click="return_handle"
-                >返回</el-button
-            >
+            <el-button class="u-return-btn" @click="return_handle"><i class="el-icon-arrow-left"></i>返回</el-button>
         </div>
         <div class="m-search">
-            <el-input
-                class="u-search-input"
-                v-model="keyword"
-                @keydown.enter.native="search_handle"
-                placeholder="输入成就名称/成就描述/称号/奖励物品「回车」进行搜索"
-            >
-            <span slot="prepend">关键词</span>
+            <el-input class="u-search-input" v-model="keyword" @keydown.enter.native="search_handle" placeholder="输入成就名称/成就描述/称号/奖励物品「回车」进行搜索">
+                <span slot="prepend"><i class="el-icon-search"></i> 关键词</span>
+                <el-button slot="append" class="u-search-btn" type="primary" plain @click="search_handle"><i class="el-icon-position"></i> 搜索</el-button>
             </el-input>
-            <el-button
-                class="u-search-btn"
-                type="primary"
-                plain
-                @click="search_handle"
-                >搜索</el-button
-            >
         </div>
     </div>
 </template>
@@ -41,7 +25,7 @@ export default {
             history.back();
         },
         search_handle() {
-            delete this.$store.state.scroll_tops['search'];
+            delete this.$store.state.scroll_tops["search"];
             this.$router.push({
                 name: "search",
                 params: { keyword: this.keyword },
@@ -52,5 +36,5 @@ export default {
 </script>
 
 <style lang="less">
-    @import '../assets/css/components/search.less';
+@import "../assets/css/components/search.less";
 </style>
